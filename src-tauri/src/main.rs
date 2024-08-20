da7a504b1024baf926b2c0589a9bdd25ed145ebc
Leone, Mark A <mleone607@gmail.com>
2024-08-19 21:37:12 -0400

    Start and stop server on backend in rust. Commented out frontend implemention that didn;t work because the windo close event was not firing

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,53 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use fix_path_env::fix;
+use std::process::{Command};
+use tauri::{WindowEvent};
+
+static PROGRAM:&str = "binaries/server-x86_64-unknown-linux-gnu";
+
+fn kill_process(pid: &str) -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(unix)]
+    {
+        println!("Sending INT signal to process with PID: {}", pid);
+
+        let mut kill = Command::new("kill")
+            .args(["-s", "SIGINT", &pid])
+            .spawn()?;
+        kill.wait()?;
+    }
+
+    #[cfg(windows)]
+    {
+        println!("Sending taskkill to process with PID: {}", pid);
+
+        let mut kill = Command::new("taskkill")
+            .args(["/PID", &pid, "/F"])
+            .spawn()?;
+        kill.wait()?;
+    }
+
+    Ok(())
+}
 
 fn main() {
-if let Err(e) = fix() {
+  if let Err(e) = fix() {
     println!("{}", e);
   } else {
+    let child = Command::new(&PROGRAM)
+        .spawn()
+        .expect("Failed to start process");
     tauri::Builder::default()
+    .on_window_event(move|event| match event.event() {
+        WindowEvent::Destroyed {  .. } => {
+          match kill_process(&child.id().to_string()) {
+            Ok(_) => println!("Process killed successfully."),
+            Err(e) => eprintln!("Failed to kill process: {}", e),
+          }
+        }
+        _ => {}
+     })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
   }
-  
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'vue3-easy-data-table/dist/style.css'
-// import './assets/style.css'
 import './assets/main.css'
 
 /* import the fontawesome core */
@@ -18,7 +17,51 @@ import { faRotate } from '@fortawesome/free-solid-svg-icons'
 
 import { useFetch } from './components/fetch.js'
 
+// import { listen } from '@tauri-apps/api/event'
+// import { appWindow } from '@tauri-apps/api/window'
+// const appWindow = window.__TAURI__.window
+// const { exit } = window.__TAURI__.process
+
 const host = 'localhost'
+
+// const CLOSE_REQUESTED = 'tauri://close-requested'
+
+// window.addEventListener('load', () => {
+//   listen('tauri://close-requested', async () => {
+//     console.log('CLOSE REQUESTED')
+//     killServer()
+
+//     // Delay closing to ensure the beacon is sent
+//     setTimeout(() => {
+//       appWindow.close()
+//     }, 1000) // Adjust the timeout as needed
+//   })
+// })
+
+// window.addEventListener('beforeunload', event => {
+//   killServer()
+//   // Optional: Prevent the default close behavior to delay the closure
+//   event.preventDefault()
+//   event.returnValue = '' // Required for Chrome
+// })
+
+// exit().then(() => {
+//   console.log('App is exiting')
+//   killServer()
+
+//   // Allow the app to exit after the beacon is sent
+//   setTimeout(() => {
+//     appWindow.close()
+//   }, 1000) // Adjust as needed
+// })
+
+// const killServer = () => {
+//   try {
+//     navigator.sendBeacon(`http://${host}:5000/kill`)
+//   } catch (e) {
+//     console.log(`Error when killing server: ${e.message}`)
+//   }
+// }
 
 const start = async () => {
   //Avoid trying to start the server again on page refresh
@@ -32,14 +75,14 @@ const start = async () => {
 }
 
 async function startServer() {
-  const Command = window.__TAURI__.shell.Command
-  const command = Command.sidecar('binaries/server')
-  try {
-    const res = await command.execute()
-    console.log(`Command result: ${JSON.stringify(res)}`)
-  } catch (e) {
-    console.log(`Error starting server: ${e.message}`)
-  }
+  // const Command = window.__TAURI__.shell.Command
+  // const command = Command.sidecar('binaries/server')
+  // try {
+  //   const res = await command.execute()
+  //   console.log(`Command result: ${JSON.stringify(res)}`)
+  // } catch (e) {
+  //   console.log(`Error starting server: ${e.message}`)
+  // }
 }
 
 const setupApp = () => {
