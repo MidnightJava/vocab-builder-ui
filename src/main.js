e279de5626660916a36e6687deb3bd9aa6f32689
Leone, Mark A <mleone607@gmail.com>
2024-08-19 21:39:37 -0400

    Removed commetned out code

--- src/main.js
+++ src/main.js
@@ -17,72 +17,16 @@ import { faRotate } from '@fortawesome/free-solid-svg-icons'
 
 import { useFetch } from './components/fetch.js'
 
-// import { listen } from '@tauri-apps/api/event'
-// import { appWindow } from '@tauri-apps/api/window'
-// const appWindow = window.__TAURI__.window
-// const { exit } = window.__TAURI__.process
-
 const host = 'localhost'
 
-// const CLOSE_REQUESTED = 'tauri://close-requested'
-
-// window.addEventListener('load', () => {
-//   listen('tauri://close-requested', async () => {
-//     console.log('CLOSE REQUESTED')
-//     killServer()
-
-//     // Delay closing to ensure the beacon is sent
-//     setTimeout(() => {
-//       appWindow.close()
-//     }, 1000) // Adjust the timeout as needed
-//   })
-// })
-
-// window.addEventListener('beforeunload', event => {
-//   killServer()
-//   // Optional: Prevent the default close behavior to delay the closure
-//   event.preventDefault()
-//   event.returnValue = '' // Required for Chrome
-// })
-
-// exit().then(() => {
-//   console.log('App is exiting')
-//   killServer()
-
-//   // Allow the app to exit after the beacon is sent
-//   setTimeout(() => {
-//     appWindow.close()
-//   }, 1000) // Adjust as needed
-// })
-
-// const killServer = () => {
-//   try {
-//     navigator.sendBeacon(`http://${host}:5000/kill`)
-//   } catch (e) {
-//     console.log(`Error when killing server: ${e.message}`)
-//   }
-// }
-
 const start = async () => {
-  //Avoid trying to start the server again on page refresh
+  // Give the backend time to start up if needed
   try {
     await useFetch(`http://${host}:5000/alive`, 'GET')
     setupApp()
   } catch (e) {
-    startServer()
-    setTimeout(() => setupApp(), 500)
+    setTimeout(() => setupApp(), 1000)
   }
-}
-
-async function startServer() {
-  // const Command = window.__TAURI__.shell.Command
-  // const command = Command.sidecar('binaries/server')
-  // try {
-  //   const res = await command.execute()
-  //   console.log(`Command result: ${JSON.stringify(res)}`)
-  // } catch (e) {
-  //   console.log(`Error starting server: ${e.message}`)
-  // }
 }
 
 const setupApp = () => {
