e8fcf1b94e50fb88722e10bfb3c0bbb07abf72b2
Leone, Mark A <mleone607@gmail.com>
2024-09-08 20:22:21 -0400

    Add log feature

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -99,7 +99,7 @@ fn main() {
         //     _ => {}
         // })
         .setup(|app| {
-            info!("Setting up the app");
+            log::info!("Setting up the app");
             let window = app.get_window("main").unwrap();
             window.show().unwrap(); // Try explicitly showing the window
             info!("Window should be visible now");
